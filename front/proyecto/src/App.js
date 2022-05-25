2869878b072b6100914df0e1baeb9c488e32d4f6
Sofia Monasterio <sofimonasterio93@gmail.com>
2022-05-25 18:14:51 -0300

    defecto footer solucionado

--- front/proyecto/src/App.js
+++ front/proyecto/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './pages/home/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/login/Login';
+import Login from './pages/login/login';
 import Account from './pages/account/Account';
 
 
