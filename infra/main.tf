287dad21de137b5170bdb230aefb79aa03cce6ba
Dionys Seidel <dionysseidel@gmail.com>
2022-05-30 14:24:57 -0300

    Mudé nuestra VPC a la región adecuada

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "g8-terraform-backend"
+    bucket         = "g8-tf-backend"
     key            = "digital-booking/terraform.tfstate" # location inside bucket. It can have several subfolders
-    region         = "us-east-1"
+    region         = "us-west-1"
     dynamodb_table = "g8-terraform-state-locking"
     encrypt        = true
     profile        = "digital_booking_g8"
@@ -16,17 +16,15 @@ terraform {
 }
 
 provider "aws" {
-  profile = var.AWS_named_profile #"digital_booking_g8"
-  region  = var.region            #"us-east-1"
-  #shared_config_files      = ["~/.aws/config"]
-  #shared_credentials_files = ["~/.aws/credentials"]
+  profile = var.AWS_named_profile
+  region  = var.region
 }
 
 module "web_app" {
   source = "./modules"
 
   region           = var.region
-  azs              = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]] #["us-east-1a", "us-east-1b"]
+  azs              = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
   public_subnets   = slice(cidrsubnets(var.main_vpc_cidr, 8, 8, 8, 8, 8, 8), 0, 2)
   private_subnets  = slice(cidrsubnets(var.main_vpc_cidr, 8, 8, 8, 8, 8, 8), 2, 4)
   database_subnets = slice(cidrsubnets(var.main_vpc_cidr, 8, 8, 8, 8, 8, 8), 4, 6)
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,2 +1,3 @@
 AWS_named_profile = "digital_booking_g8"
 app_name          = "digital-booking"
+region            = "us-west-1"
