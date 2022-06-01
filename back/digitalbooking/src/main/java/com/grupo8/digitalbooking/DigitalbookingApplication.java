99a6bbefed31bba6b8ffb6510d492b9a201a1856
Diana <dianasauval@hotmail.com>
2022-06-01 12:57:45 -0300

    conectando a la bbdd

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
@@ -18,7 +18,7 @@ public class DigitalbookingApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/**");
 			}
 		};
 	}
