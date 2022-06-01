7a8855a9d32d6dd94f283063b27c7a6410e666d6
Ale Pantano <alejandra_pantano@hotmail.com>
2022-06-01 20:04:50 -0300

    Politicas del producto

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/model/Politicas/PoliticaProd.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/model/Politicas/PoliticaProd.java
@@ -1,4 +1,45 @@
 package com.grupo8.digitalbooking.model.Politicas;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+//Crear una tabla categorias en la base de datos
+@ToString
+@Getter
+@Setter
+
+//nombre de la tabla en la bd
+@Entity
+@Table(name = "politicasDelProducto")
 public class PoliticaProd {
+
+    @Id
+    @SequenceGenerator(name = "politicaProd_sequence", sequenceName = "politicaProd_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "politicaProd_sequence")
+
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "tipoDePolitica_id", nullable = false)
+    private TipoDePolitica tipoDePolitica;
+
+    //constructor vacio
+    public PoliticaProd() {
+    }
+
+    //constructor SIN id
+    public PoliticaProd(TipoDePolitica tipoDePolitica) {
+        this.tipoDePolitica = tipoDePolitica;
+    }
+
+    //constructor CON id
+    public PoliticaProd(Integer id, TipoDePolitica tipoDePolitica) {
+        this.id = id;
+        this.tipoDePolitica = tipoDePolitica;
+    }
+
+
 }
---
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/repository/Politicas/TipoDePoliticaRepository.java
@@ -0,0 +1,7 @@
+package com.grupo8.digitalbooking.repository.Politicas;
+
+import com.grupo8.digitalbooking.model.Politicas.TipoDePolitica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoDePoliticaRepository extends JpaRepository<TipoDePolitica, Integer> {
+}
