8b61996b34c28466f4adf6a1464022d19e89139d
Sofia Monasterio <sofimonasterio93@gmail.com>
2022-06-01 11:29:21 -0300

    Agrego clase Imagen c/ repository, service y controller

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/model/Imagen.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/model/Imagen.java
@@ -1,4 +1,36 @@
 package com.grupo8.digitalbooking.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@ToString
+@Getter
+@Setter
+
+@Entity
+@Table(name = "imagenes")
 public class Imagen {
+    @Id
+    @SequenceGenerator(name = "imagen_sequence", sequenceName = "imagen_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "imagen_sequence")
+    private Integer ID;
+    private String nombre;
+    private String URL;
+
+    public Imagen(Integer ID, String nombre, String URL) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.URL = URL;
+    }
+
+    public Imagen(String nombre, String URL) {
+        this.nombre = nombre;
+        this.URL = URL;
+    }
+
+    public Imagen() {
+    }
 }
