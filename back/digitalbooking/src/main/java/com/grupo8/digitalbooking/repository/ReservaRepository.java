e99a1282206bb21fd22b73386048a89555df697a
Ale Pantano <alejandra_pantano@hotmail.com>
2022-06-16 16:07:51 -0300

    ap properties y util

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/repository/ReservaRepository.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/repository/ReservaRepository.java
@@ -4,4 +4,5 @@ import com.grupo8.digitalbooking.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
+
 }
--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/util/Util.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/util/Util.java
@@ -1,2 +1,11 @@
-package com.grupo8.digitalbooking.util;public class Util {
+package com.grupo8.digitalbooking.util;
+
+import java.util.Date;
+
+public class Util {
+    public static java.sql.Date utilDateToSqlDate(Date utilDate){
+        long timeInMilliSeconds = utilDate.getTime();
+        java.sql.Date sqlDate = new java.sql.Date(timeInMilliSeconds);
+        return sqlDate;
+    }
 }
