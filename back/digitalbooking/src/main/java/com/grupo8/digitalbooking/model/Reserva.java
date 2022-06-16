81758d63b1798f956cda4c865dbbdeb7ab1ce927
Sergio Andres Perez Amsler <sergioperezamsler@gmail.com>
2022-06-16 11:01:24 -0300

    reserva entidad

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/model/Reserva.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/model/Reserva.java
@@ -15,42 +15,42 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "reservas")
-public class Reservas {
+public class Reserva {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer id;
-        private Time Hora;
-        private LocalDate FechaInicial;
-        private LocalDate FechaFinal;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Time Hora;
+    private LocalDate FechaInicial;
+    private LocalDate FechaFinal;
 
-        @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-        @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-        @JoinColumn(name = "productos_id")
-        private Producto producto;
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JoinColumn(name = "productos_id")
+    private Producto producto;
 
-        @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-        @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-        @JoinColumn(name = "usuario_id")
-        private Usuarios usuario;
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
-        public Reservas(Time hora, LocalDate fechaInicial, LocalDate fechaFinal, Producto producto, Usuarios usuario) {
-                Hora = hora;
-                FechaInicial = fechaInicial;
-                FechaFinal = fechaFinal;
-                this.producto = producto;
-                this.usuario = usuario;
-        }
+    public Reserva(Time hora, LocalDate fechaInicial, LocalDate fechaFinal, Producto producto, Usuario usuario) {
+        Hora = hora;
+        FechaInicial = fechaInicial;
+        FechaFinal = fechaFinal;
+        this.producto = producto;
+        this.usuario = usuario;
+    }
 
-        public Reservas(Integer id, Time hora, LocalDate fechaInicial, LocalDate fechaFinal, Producto producto, Usuarios usuario) {
-                this.id = id;
-                Hora = hora;
-                FechaInicial = fechaInicial;
-                FechaFinal = fechaFinal;
-                this.producto = producto;
-                this.usuario = usuario;
-        }
+    public Reserva(Integer id, Time hora, LocalDate fechaInicial, LocalDate fechaFinal, Producto producto, Usuario usuario) {
+        this.id = id;
+        Hora = hora;
+        FechaInicial = fechaInicial;
+        FechaFinal = fechaFinal;
+        this.producto = producto;
+        this.usuario = usuario;
+    }
 
-        public Reservas() {
-        }
+    public Reserva() {
+    }
 }
