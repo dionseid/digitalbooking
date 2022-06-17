15ed6715eb0476b7e6a3fe5a73de025c192eabea
Sergio Andres Perez Amsler <sergioperezamsler@gmail.com>
2022-06-17 10:15:21 -0300

    prueba security

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/repository/UsuarioRepository.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/repository/UsuarioRepository.java
@@ -3,5 +3,6 @@ package com.grupo8.digitalbooking.repository;
 import com.grupo8.digitalbooking.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 }
