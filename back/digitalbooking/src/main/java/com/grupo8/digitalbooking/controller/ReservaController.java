8ac37fc3723728dff4cf013565739e936ce6f131
Diana <dianasauval@hotmail.com>
2022-06-21 16:12:01 -0300

    agreado crossOrigin en reservaController

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/controller/ReservaController.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/controller/ReservaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reserva")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ReservaController {
     @Autowired
     private ReservaService reservaService;
