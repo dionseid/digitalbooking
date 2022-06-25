80956db2151f10a1cbd35c03afd14711d53c4004
Dionys Seidel <dionysseidel@gmail.com>
2022-06-25 14:07:34 -0300

    Especifico ruta larga para resolver rules:changes

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
@@ -27,7 +27,7 @@ public class DigitalbookingApplication {
 						.allowedOrigins("/**")
 						.allowedMethods("*")
 						.allowedHeaders("*")
-						.somethingHere();
+						.somethingHere(hey);
 			}
 		};
 	}
