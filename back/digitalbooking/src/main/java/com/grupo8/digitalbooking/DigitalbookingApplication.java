5e7fc332b2edff57f4b979d51494a051a85c8120
Dionys Seidel <dionysseidel@gmail.com>
2022-06-25 16:48:43 -0300

    Conf de CORS que sirvió en app -v 1140

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
@@ -23,8 +23,8 @@ public class DigitalbookingApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("/**")
+				registry.addMapping("*")
+						.allowedOrigins("*")
 						.allowedMethods("*")
 						.allowedHeaders("*");
 			}
