901ceeeda55f481f41f5a0d782c6f2c612fce920
Dionys Seidel <dionysseidel@gmail.com>
2022-06-26 23:16:43 -0300

    Vuelvo a probar CORS con addMapping("/**")

--- back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
+++ back/digitalbooking/src/main/java/com/grupo8/digitalbooking/DigitalbookingApplication.java
@@ -23,7 +23,7 @@ public class DigitalbookingApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*")
+				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.allowedMethods("*")
 						.allowedHeaders("*");
