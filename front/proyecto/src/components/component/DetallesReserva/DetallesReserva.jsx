a5ab53209fb772b9a250fc2ed1caab29791dc0af
Diana <dianasauval@hotmail.com>
2022-07-01 16:40:16 -0300

    agregando validaciones del login de la rama de franco

--- front/proyecto/src/components/component/DetallesReserva/DetallesReserva.jsx
+++ front/proyecto/src/components/component/DetallesReserva/DetallesReserva.jsx
@@ -9,20 +9,22 @@ import FechaRangoContextProvider from "../../context/FechaRangoContextProvider";
 import HoraContextProvider from "../../context/HoraContextProvider";
 import UserProvider from "../../context/UserContext";
 import axiosConnection from "../../../helpers/axiosConnection";
+import axios from "axios";
 
 export default function DetallesReserva() {
   const { user } = useContext(UserProvider);
   const { isHora, setIsHora } = useContext(HoraContextProvider);
   const { rango, setRango } = useContext(FechaRangoContextProvider);
-  console.log("rango: ", rango);
+  //console.log("rango: ", rango);
   const [dataProducto, setDataProducto] = useState([]);
   const [dataImagen, setDataImagen] = useState([]);
   const [isCiudad, setIsCiudad] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
   const { id } = useParams();
   const navigate = useNavigate();
+  var cors = require('cors')
 
-  console.log("isCiudad: ", isCiudad);
+  //console.log("isCiudad: ", isCiudad);
   useEffect(() => {
     if (user.ciudad !== "") {
       setIsCiudad(true);
@@ -70,21 +72,8 @@ export default function DetallesReserva() {
     }
   };
 
-  console.log("isHora:", isHora);
-  const getLoginApi = async () => {
-    try {
-      // ** CAMBIAR POR EL URL DE LA API
-      // const token = sessionStorage.getItem('token')&& JSON.parse(sessionStorage.getItem('token'));
-      const respuesta = await axiosConnection.get("/login", {
-        headers: {
-          Authorization: `Bearer `,
-        },
-      });
-      return respuesta;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //console.log("isHora:", isHora);
+  
 
   useEffect(() => {
     if (rango[0] !== null && rango[1] !== null && isHora!==null && isCiudad) {
@@ -94,22 +83,66 @@ export default function DetallesReserva() {
     }
   }, [rango, isHora,isCiudad])
 
+  const registroReserva = async (data) => {
+    let corsOptions = {
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+    }
+    const token = sessionStorage.getItem('token')
+    //JSON.parse(sessionStorage.getItem('token'))
+    console.log(token);
+    try {
+        const respuesta = await axios.post("http://localhost:8080/reserva/nuevaReserva",cors(corsOptions),data,{
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        if (respuesta.status !== 200) {
+            throw new Error("Lamentablemente no se ha podido crear la reserva. Por favor intente más tarde")
+        } else {
+            console.log("respuesta1: ", respuesta.data);
+        }
+        return respuesta.data;
+    } catch (error) {
+        console.error("ERROR REGISTRO RESERVA ", error);
+    }
+  }
+
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const newReserva = {
+      hora: "15:00:00",
+      fechaInicial: fechaInicio,
+      fechaFinal: fechaFinal,
+      producto: {
+        id: parseInt(id),
+      },
+      usuario: {
+        id: user.id,
+      }
+    };
+
+    console.log(newReserva);
+    
     if (rango[0] !== null && rango[1] !== null && isHora && isCiudad) {
+      registroReserva(newReserva)
+      /* const token = sessionStorage.getItem('token') */
       // if(getLoginApi().status === 200 ){
-/*       fetch("http://localhost:8080/reserva/nuevaReserva", {
+      /* fetch("http://localhost:8080/reserva/nuevaReserva", {
         method: "POST",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
         },
         body: JSON.stringify({
           hora: "15:00:00",
           fechaInicial: fechaInicio,
           fechaFinal: fechaFinal,
           producto: {
-            id: id,
+            id: parseInt(id),
           },
           usuario: {
             id: user.id,
