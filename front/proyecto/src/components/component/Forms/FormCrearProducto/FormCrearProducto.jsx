b46621fd524798f118c3330a7ab88a1cbf0efd6b
Diana <dianasauval@hotmail.com>
2022-07-04 18:35:26 -0300

    agregado el navigate a creacionProductoExitosa

--- front/proyecto/src/components/component/Forms/FormCrearProducto/FormCrearProducto.jsx
+++ front/proyecto/src/components/component/Forms/FormCrearProducto/FormCrearProducto.jsx
@@ -472,7 +472,7 @@ export default function FormCrearProducto() {
       
       peticionCaracteristicas(idProducto)      
 
-      //navigate("/creacionExitosa");
+      navigate("/creacionExitosa");
     }
   };
 
