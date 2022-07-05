14ebd59921c47f978e10543c21c7135bdb7bc453
Dionys Seidel <dionysseidel@gmail.com>
2022-07-05 19:19:55 -0300

    Front apunta a https://remo-digitalbooking.click

--- front/proyecto/src/helpers/axiosConnection.js
+++ front/proyecto/src/helpers/axiosConnection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConnection = axios.create({
-    baseURL: "https://remo-digitalbooking-env-prod.eba-xby23mds.us-west-1.elasticbeanstalk.com/",
+    baseURL: "https://remo-digitalbooking.click",
     headers: {
         "Content-type": "application/json",
         "Accept": "application/json"
